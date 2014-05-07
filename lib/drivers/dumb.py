f55b8cb105eed8ed8ae624461e432eb341fd6a44
dmp42 <olivier@webitup.fr>
2014-05-07 21:54:05 +0200

    Get in

---
+++ lib/drivers/dumb.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+"""
+droid.core.drivers.dumb
+~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+This is a very dumb driver, which uses memory to store data.
+It obviously won't work out of very simple tests.
+Should only be used as inspiration
+
+"""
+
+from droid.core import driver
+from droid.core.exceptions import FileNotFoundError
+
+
+class Storage(driver.Base):
+
+    _storage = {}
+
+    def __init__(self):
+        pass
+
+    def exists(self, path):
+        return path in self._storage
+
+    def get_size(self, path):
+        if path not in self._storage:
+            raise FileNotFoundError('%s is not there' % path)
+        return len(self._storage[path])
+
+    def get_content(self, path):
+        if path not in self._storage:
+            raise FileNotFoundError('%s is not there' % path)
+        return self._storage[path]
+
+    def put_content(self, path, content):
+        self._storage[path] = content
+
+    def remove(self, path):
+        if path not in self._storage:
+            raise FileNotFoundError('%s is not there' % path)
+        del self._storage[path]
+
+    # def stream_read(self, path, bytes_range=None):
+
+    # def stream_write(self, path, fp):
+    #     """
+    #     Method to stream write
+    #     """
+    #     raise NotImplementedError(
+    #         "You must implement stream_write(self, path, fp) " +
+    #         "on your storage %s" %
+    #         self.__class__.__name__)
+
+    # def list_directory(self, path=None):
+    #     """
+    #     Method to list directory
+    #     """
+    #     raise NotImplementedError(
+    #         "You must implement list_directory(self, path=None) " +
+    #         "on your storage %s" %
+    #         self.__class__.__name__)
---
+++ setup.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+try:
+    import setuptools
+except ImportError:
+    import distutils.core as setuptools
+
+import sys
+
+from lib.core import __author__
+from lib.core import __email__
+from lib.core import __maintainer__
+from lib.core import __title__
+from lib.core import __version__
+
+if sys.version_info < (2, 6):
+    raise Exception("Docker registry requires Python 2.6 or higher.")
+
+requirements_txt = open('./requirements.txt')
+requirements = [line for line in requirements_txt]
+
+ver = sys.version_info
+
+# 2.5 doesn't have json and 2.6 native json raw_decode doesn't fit the bill
+if ver[0] == 2 and ver[1] <= 6:
+    requirements.insert(0, 'simplejson')
+
+packages = ['droid', 'droid.core', 'droid.drivers']
+
+setuptools.setup(
+    name=__title__,
+    version=__version__,
+    author=__author__,
+    author_email=__email__,
+    maintainer=__maintainer__,
+    maintainer_email=__email__,
+    url='https://github.com/dmp42/droid-core',
+    description="Backend drivers for the docker registry",
+    long_description=open('./README.md').read(),
+    download_url='https://github.com/dmp42/droid-core/archive/master.zip',
+    classifiers=['Development Status :: 4 - Beta',
+                 'Intended Audience :: Developers',
+                 'Programming Language :: Python',
+                 'Programming Language :: Python :: 2.6',
+                 'Programming Language :: Python :: 2.7',
+                 'Programming Language :: Python :: 3.2',
+                 'Programming Language :: Python :: 3.3',
+                 'Programming Language :: Python :: Implementation :: CPython',
+                 'Programming Language :: Python :: Implementation :: PyPy',
+                 'Operating System :: OS Independent',
+                 'Topic :: Utilities',
+                 'License :: OSI Approved :: Apache Software License'],
+    # platforms=['Independent'],
+    license=open('./LICENSE').read(),
+    namespace_packages=['droid', 'droid.drivers'],
+    package_dir={'droid': 'lib'},
+    packages=packages,
+    install_requires=requirements,
+    zip_safe=True,
+    setup_requires=['nose', 'flake8'],
+    test_suite='nose.collector'
+)
